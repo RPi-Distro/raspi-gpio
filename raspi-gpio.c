f9f33fcbfe15804d3b6e213aca495f7567fda5f5
Ryan Whelan <4249368+rwhelan@users.noreply.github.com>
2019-12-04 18:19:45 +0000

    feat: Show Pull registers on raw output on Pi4

--- raspi-gpio.c
+++ raspi-gpio.c
@@ -395,10 +395,17 @@ int gpio_fsel_to_namestr(int gpio, int fsel, char *name)
 void print_raw_gpio_regs(void)
 {
     int i;
-    int end = is_2711 ? GPPUPPDN3 : GPPUDCLK1;
 
-    for (i = 0; i <= end; i++)
+    for (i = 0; i <= GPPUPPDN3; i++)
     {
+        /* Nothing interesting past GPPUDCLK1 for non-Pi4 models */
+        if (i > GPPUDCLK1 && !is_2711)
+            break;
+
+        /* Skip over non-GPIO registers on Pi4 models */
+        if (i > GPPUDCLK1 && i < (GPPUPPDN0 - 1))
+            continue;
+
         uint32_t val = *(gpio_base + i);
         if ((i & 3) == 0)
             printf("%02x:", i * 4);
